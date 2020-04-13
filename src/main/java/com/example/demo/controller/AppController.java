84588ba8eb57f764ef615a99a8a67302a510eace
hiroaki maruoka <dm.hiro@gmail.com>
2020-04-13 22:32:43 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/AppController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+    @RequestMapping("/")
+    String index(Model model){
+        model.addAttribute("message","hello world");
+        return "index";
+    }
+}
