9edba6e19f9b0bdc58a52a0be008705a4be5ec1c
hiroaki maruoka <dm.hiro@gmail.com>
2020-04-14 10:10:24 +0900

    サンプルテーブルの作成と表示

--- src/main/java/com/example/demo/controller/AppController.java
+++ src/main/java/com/example/demo/controller/AppController.java
@@ -1,14 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class AppController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @RequestMapping("/")
-    String index(Model model){
-        model.addAttribute("message","hello world");
+    String index(Model model) {
+        List<Map<String, Object>> users = jdbcTemplate.queryForList("select * from sample.users");
+        model.addAttribute("title", "ユーザ一覧");
+        model.addAttribute("users", users);
         return "index";
     }
 }
